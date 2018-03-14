5610c7c06556b8042515bfbf49f416f3c27388b3
Henrique Pappis <henrique.pappis@simers.org.br>
2018-03-14 09:43:24 -0300

    Ajuste conexão banco de dados

--- db.php
+++ db.php
@@ -1,18 +1,17 @@
 <?php
-    $username = "henriqu7";
+    $username = "dica";
     $password = "dica300986";
     try {
-        $conn = new PDO('mysql:host=155.94.64.210;dbname=henriqu7_site', $username, $password);
+        $conn = new PDO('mysql:host=mysql524.umbler.com;dbname=henriquepappis', $username, $password);
         $conn->exec("set names utf8");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e) {
         echo 'ERROR: ' . $e->getMessage();
     }
-    
+
     function devolveLinha($sql){
         $result = mysql_query($query) or die('Falha na instrução SQL: ' . mysql_error());
         # Filtra através das linhas de consulta
         $row = mysql_fetch_object($result);
     }
-
-?> 
+?>
