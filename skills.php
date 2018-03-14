9ecd8010421dabb2840058d94b3a3bc588f00414
Henrique Pappis <henriquepappis@MacBookHenrique.local>
2018-03-14 20:50:58 -0300

    ajustes

--- skills.php
+++ skills.php
@@ -52,3 +52,4 @@
                         </div><!--//content-->  
                     </div><!--//section-inner-->                 
                 </aside><!--//section-->
+<?php $conn->close();?>
